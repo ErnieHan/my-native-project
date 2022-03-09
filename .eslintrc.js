8c3f9d98628221ae6c19b771656f4403c6a72018
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2022-03-09 16:01:18 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: '@react-native-community',
+    rules: {
+        'prettier/prettier': 'error',
+        'no-console': 'off',
+        'jsx-a11y/anchor-is-valid': 'off',
+        'jsx-a11y/href-no-hash': 'on'
+    }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'avoid',
+    tabWidth: 4,
+    printWidth: 100,
+    semi: false
+}
